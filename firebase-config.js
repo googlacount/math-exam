3c36e9d5f5f63aa3f886ef8dcdba3c6abc3d7c39
Mo_Mady <googlacount2021@gmail.com>
2025-11-10 01:42:07 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAJdFkBMrroe-ikfXLrMGQ-EJCY_8671iU",
+  authDomain: "wallpaper-admin-940e5.firebaseapp.com",
+  databaseURL: "https://wallpaper-admin-940e5-default-rtdb.firebaseio.com",
+  projectId: "wallpaper-admin-940e5",
+  storageBucket: "wallpaper-admin-940e5.appspot.com",
+  messagingSenderId: "793347884499"
+};
